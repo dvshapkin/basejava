d5d5d7a826d43563bcc9245c122a01874debfa9d
Dmitry <dvshapkin@mail.ru>
2018-09-04 12:55:56 +0700

    реализован класс ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,56 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final int MAX_RESUME_COUNT = 10_000;
+    private int size = 0;
+
+    private Resume[] storage = new Resume[MAX_RESUME_COUNT];
 
     void clear() {
+        Arrays.fill(storage, 0, size-1, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (size > MAX_RESUME_COUNT) throw new ArrayIndexOutOfBoundsException("The number of resumes is too large");
+        storage[size++] = resume;
     }
 
     Resume get(String uuid) {
-        return null;
+        int idx = indexOf(uuid);
+        if (idx < 0) return null;
+        return storage[idx];
     }
 
     void delete(String uuid) {
+        int idx = indexOf(uuid);
+        if (idx < 0) return;
+        for (int i = idx; i < size - 1; ++i) {
+            storage[i] = storage[i + 1];
+        }
+        storage[--size] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int indexOf(String uuid) {
+        for (int i = 0; i < size; ++i) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
